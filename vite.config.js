5413419cd1cc55b6e1100266e81ecc591108085b
lehung2022 <hung.le.programmer@gmail.com>
2024-11-12 08:52:48 +0700

    fiexed the config file

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.rawg.io",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    // Development-only settings
+    return {
+      server: {
+        proxy: {
+          "/api": {
+            target: "https://api.rawg.io",
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ""),
+          },
+        },
       },
-    },
-  },
-  plugins: [react()],
+      plugins: [react()],
+    };
+  } else {
+    // Production settings (no proxy)
+    return {
+      plugins: [react()],
+    };
+  }
 });
